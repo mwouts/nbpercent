ee02c05d61ec14fa59eeb44aaaf073bb393ef5cf
Marc Wouts <marc.wouts@gmail.com>
2022-04-21 10:30:49 +0200

    Ignore .ipynb_checkpoints folder

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,8 +8,9 @@ def pytest_generate_tests(metafunc):
         ids = []
         ipynb_notebook_paths = []
         for ipynb_notebook_path in (Path(__file__).parent / "notebooks").iterdir():
-            ipynb_notebook_paths.append(ipynb_notebook_path)
-            ids.append(ipynb_notebook_path.stem)
+            if ipynb_notebook_path.is_file():
+                ipynb_notebook_paths.append(ipynb_notebook_path)
+                ids.append(ipynb_notebook_path.stem)
 
         metafunc.parametrize("ipynb_notebook_path", ipynb_notebook_paths, ids=ids)
 
